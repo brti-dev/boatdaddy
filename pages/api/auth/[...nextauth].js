2e498f22387dea16f6867acb814b8ee9360386b4
dr-spaceman <mat.berti@gmail.com>
2021-07-14 12:43:50 +0800

    Prisma setup

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,15 +2,15 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 export default NextAuth({
-    // Configure one or more authentication providers
-    providers: [
-        Providers.Facebook({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        }),
-        // ...add more providers here
-    ],
+  // Configure one or more authentication providers
+  providers: [
+    Providers.Facebook({
+      clientId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+    }),
+    // ...add more providers here
+  ],
 
-    // A database is optional, but required to persist accounts in a database
-    database: process.env.DATABASE_URL,
+  // A database is optional, but required to persist accounts in a database
+  database: process.env.DATABASE_URL,
 })
