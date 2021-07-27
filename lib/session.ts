bbf731ead9104d3c6386ebf8ce356bfeb58a9a95
Matt Berti <mat.berti@gmail.com>
2021-07-27 15:38:06 +0800

    Narrow Session type

--- lib/session.ts
+++ lib/session.ts
@@ -1,9 +1,20 @@
+export type Identity = {
+  name?: string
+  username?: string
+  birthday?: string
+  isDaddy?: Boolean
+  hasBoat?: Boolean
+  createdAt?: string
+  updatedAt?: string
+}
+
 export type Session = {
   user?: {
+    id?: number | null
     name?: string | null
     email?: string | null
     image?: string | null
-    identity?: object | null
+    identity?: Identity
   }
   expires?: string
 }
