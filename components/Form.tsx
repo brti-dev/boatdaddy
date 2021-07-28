7546fdd369fdb2767a7080f18e494ab5a9620b9a
dr-spaceman <mat.berti@gmail.com>
2021-07-28 18:28:28 +0800

    Form component  error and helperText

--- components/Form.tsx
+++ components/Form.tsx
@@ -27,18 +27,27 @@ export type FormGroupProps = {
   label: string
   input: React.ReactElement
   className?: string
+  error?: boolean
+  helperText?: string
 } & React.ComponentPropsWithoutRef<'div'>
 
-export function FormGroup({ label, input, className = null }: FormGroupProps) {
-  const classNameString = [classes.formGroup, className]
-    .filter(i => !!i)
-    .join(' ')
+export function FormGroup({
+  label,
+  input,
+  className = null,
+  error = false,
+  helperText = null,
+}: FormGroupProps) {
+  const classNames = [classes.formGroup, className]
+  if (error) classNames.push(classes.error)
+  const classNameString = classNames.filter(i => !!i).join(' ')
   const id = `form__${label}`
 
   return (
     <div className={classNameString}>
       <label htmlFor={id}>{label}</label>
       {cloneElement(input, { id })}
+      {helperText && <p>{helperText}</p>}
     </div>
   )
 }
