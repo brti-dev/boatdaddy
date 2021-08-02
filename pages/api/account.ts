703e17cbf8f6d37b8cdce7ddb8b284f9c5f7d499
Matt Berti <mat.berti@gmail.com>
2021-08-02 13:54:53 +0800

    Update Actor type in schema and account api

--- pages/api/account.ts
+++ pages/api/account.ts
@@ -1,7 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/client'
-import prisma from '@/lib/prisma'
 import { Session } from '@/lib/session'
+import prisma from '@/lib/prisma'
+
+console.log({ prisma })
 
 export default async function handle(
   req: NextApiRequest,
@@ -21,56 +23,60 @@ export default async function handle(
 
   // res.status(200).json({ reqBody: req.body, reqOperation: operation })
 
-  switch (req.method) {
-    case 'PUT':
-      // Create account
-      const roles = [{ role: 'RIDER' }]
-      if (data.hasBoat) roles.push({ role: 'DRIVER' })
+  // Create account
+  if (req.method === 'PUT') {
+    const roles = [{ role: 'RIDER' }]
+    if (data.hasBoat) roles.push({ role: 'DRIVER' })
 
-      const putOperation = {
-        data: {
-          ...data,
-          actor: { create: roles },
-          User: { connect: { id: session.user.id } },
-        },
-      }
-      const putResult = await prisma.identity.create(putOperation)
+    const putOperation = {
+      data: {
+        ...data,
+        actor: { create: roles },
+        User: { connect: { id: session.user.id } },
+      },
+    }
+    const putResult = await prisma.identity.create(putOperation)
 
-      res.json(putResult)
+    res.json(putResult)
 
-      break
-
-    case 'POST':
-      // Update account
-      const allRoles = await prisma.actor.findMany({
-        where: { userId: session.user.id },
-      })
-      console.log({ allRoles })
-
-      let createRoles: string[]
-
-      // await prisma.actor.create({
-      //   data: { role: 'RIDER', userId: session.user.id },
-      // })
-      // if (data.hasBoat) {
-      //   await prisma.actor.create({
-      //     data: { role: 'DRIVER', userId: session.user.id },
-      //   })
-      // }
-
-      const postOperation = {
-        data,
-        where: { userId: session.user.id },
-      }
-      const postResult = await prisma.identity.update(postOperation)
-
-      res.json(postResult)
-
-      break
-
-    default:
-      res.status(405).json({
-        message: `The requested method ${req.method} is not supported`,
-      })
+    return
   }
+
+  // Update account
+  if (req.method === 'POST') {
+    const allActor = await prisma.actor.findMany({
+      where: { userId: session.user.id },
+    })
+    console.log({ allActor })
+
+    let roles: string[] = []
+
+    if (data.hasBoat) {
+      const driverRole = allActor.filter(actor => actor.role === 'DRIVER')
+      if (!driverRole) {
+        roles.push('DRIVER')
+      }
+    }
+
+    if (roles.length) {
+      const actorResult = await prisma.actor.create({
+        data: { role: 'DRIVER', userId: session.user.id },
+      })
+      console.log('Actor', actorResult)
+    }
+
+    const postOperation = {
+      data,
+      where: { userId: session.user.id },
+    }
+    const postResult = await prisma.identity.update(postOperation)
+
+    res.json(postResult)
+
+    return
+  }
+
+  res.status(405).json({
+    message: `The requested method ${req.method} is not supported`,
+  })
 }
