61d57824eaa8e8f92e612f1a1d33a4c53f3d917e
dr-spaceman <mat.berti@gmail.com>
2021-08-03 21:44:29 +0800

    TextInput component multiline support

--- components/Form.tsx
+++ components/Form.tsx
@@ -62,70 +62,67 @@ function unformat(text: string) {
 
 export type TextInputProps = {
   type?: string
-  tag?: 'input' | 'textarea'
   name?: string
   value?: string
+  multiline?: boolean
+  rows?: number
+  // maxRows?: number
   onChange?: (event: any, text: string) => void
-} & Omit<JSX.IntrinsicElements['input'], 'onChange'> // Necessary because of some strange error
+} & Omit<JSX.IntrinsicElements['input'], 'onChange'> & // Necessary because of some strange error
+  Omit<JSX.IntrinsicElements['textarea'], 'onChange'>
 
-export const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
-  (props, ref) => {
-    const {
-      type = 'text',
-      tag = 'input',
-      name,
-      value,
-      onChange = null,
-      ...fieldProps
-    } = props
-    const [textValue, setTextValue] = useState<string | null>(format(value))
+export const TextInput = forwardRef<
+  HTMLTextAreaElement & HTMLInputElement,
+  TextInputProps
+>((props, ref) => {
+  const {
+    type = 'text',
+    name,
+    value,
+    multiline = false,
+    rows = 1,
+    // maxRows,
+    onChange = null,
+    ...fieldProps
+  } = props
+  const [textValue, setTextValue] = useState<string | null>(format(value))
 
-    const handleBlur = (event: SyntheticEvent) => {
-      if (onChange) {
-        onChange(event, unformat(textValue))
-      }
+  const handleBlur = (event: SyntheticEvent) => {
+    if (onChange) {
+      onChange(event, unformat(textValue))
     }
-
-    const handleChange = (event: SyntheticEvent) => {
-      const { value } = event.target as HTMLInputElement
-      setTextValue(value)
-    }
-
-    if (tag === 'input') {
-      return (
-        <input
-          type={type}
-          {...fieldProps}
-          name={name}
-          value={textValue}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          ref={ref}
-          className="input"
-        />
-      )
-    }
-
-    // if (tag === 'textarea') {
-    //   return (
-    //     <textarea
-    //       {...fieldProps}
-    //       name={name}
-    //       onBlur={handleBlur}
-    //       onChange={handleChange}
-    //       rows={4}
-    //     >
-    //       {textValue}
-    //     </textarea>
-    //   )
-    // }
-
-    // // What other tag types...?
-    // return createElement(tag, {
-    //   ...fieldProps,
-    //   value: textValue,
-    //   onBlur: handleBlur,
-    //   onChange: handleChange,
-    // })
   }
-)
+
+  const handleChange = (event: SyntheticEvent) => {
+    const { value } = event.target as HTMLInputElement
+    setTextValue(value)
+  }
+
+  if (multiline) {
+    return (
+      <textarea
+        {...fieldProps}
+        name={name}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        rows={rows}
+        ref={ref}
+        className="input"
+        value={textValue}
+      />
+    )
+  }
+
+  return (
+    <input
+      type={type}
+      {...fieldProps}
+      name={name}
+      value={textValue}
+      onBlur={handleBlur}
+      onChange={handleChange}
+      ref={ref}
+      className="input"
+    />
+  )
+})
