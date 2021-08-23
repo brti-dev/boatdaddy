7b173bdb05c8af11aa53439fcb780bca928829b9
dr-spaceman <mat.berti@gmail.com>
2021-08-23 19:15:44 +0800

    Image schema and resolvers

---
+++ lib/graphql/image.ts
@@ -0,0 +1,33 @@
+import {
+  Field,
+  ObjectType,
+  Int,
+  Mutation,
+  Authorized,
+  Resolver,
+} from 'type-graphql'
+const cloudinary = require('cloudinary').v2
+
+@ObjectType()
+class ImageSignature {
+  @Field(_type => String)
+  signature!: string
+
+  @Field(_type => Int)
+  timestamp!: number
+}
+
+@Resolver()
+export class ImageResolver {
+  @Authorized()
+  @Mutation(_returns => ImageSignature)
+  createImageSignature(): ImageSignature {
+    const timestamp = Math.round(new Date().getTime() / 1000)
+    const signature: string = cloudinary.utils.api_sign_request(
+      { timestamp },
+      process.env.CLOUDINARY_SECRET
+    )
+
+    return { timestamp, signature }
+  }
+}
