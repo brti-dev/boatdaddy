1a7fe63adf47d48ff8c38d1359f54e4da150a53e
Matt Berti <mat.berti@gmail.com>
2021-11-13 22:57:13 +0800

    GraphQL asyn client and hooks

---
+++ src/graphql/fetch.ts
@@ -0,0 +1,71 @@
+import { DocumentNode } from 'graphql'
+
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d')
+
+function jsonDateReviver(key, value) {
+  if (dateRegex.test(value)) {
+    return new Date(value)
+  }
+
+  return value
+}
+
+/**
+ * Parse the first GraphQL error
+ *
+ * @returns {string} A stringified summary of the first error encountered
+ */
+function parseErrors(errors): string {
+  if (errors && errors[0]) {
+    const error = errors[0]
+    if (error.extensions.code === 'BAD_USER_INPUT') {
+      const details = error.extensions.exception.errors.join('\n ')
+      return `${error.message}: ${details}`
+    }
+
+    return `${error.extensions.code}: ${error.message}`
+  }
+
+  return null
+}
+
+/**
+ * Common utility function to handle API calls
+ */
+async function graphQlFetch(
+  query: string | DocumentNode,
+  variables = {},
+  cookie: string = null
+) {
+  const headers: any = { 'Content-Type': 'application/json' }
+
+  const jwt = localStorage.getItem('jwt')
+  if (jwt) {
+    headers.Authorization = `Bearer ${jwt}`
+  }
+
+  if (cookie) {
+    headers.Cookie = cookie
+  }
+
+  const response = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT, {
+    method: 'POST',
+    credentials: 'include',
+    headers,
+    body: JSON.stringify({ query, variables }),
+  })
+  const body = await response.text()
+  const result = JSON.parse(body, jsonDateReviver)
+
+  if (!result) {
+    throw new Error('GraphQL fetch returned an empty result')
+  }
+
+  if (result.error) {
+    throw new Error(parseErrors(result.error))
+  }
+
+  return result
+}
+
+export default graphQlFetch
---
+++ src/graphql/hooks.tsx
@@ -0,0 +1,115 @@
+import { useState, useEffect } from 'react'
+
+import graphQlFetch from './fetch'
+
+type HookState = {
+  loading: boolean
+  data: null | object
+  error: null | string
+}
+
+/**
+ * Parse the first GraphQL error
+ *
+ * @param {object} errors GraphQL `errors` response
+ *
+ * @returns {string} A stringified summary of the first error encountered
+ */
+function parseErrors(errors) {
+  if (errors && errors[0]) {
+    const error = errors[0]
+    if (error.extensions.code === 'BAD_USER_INPUT') {
+      const details = error.extensions.exception.errors.join('\n ')
+      return `${error.message}: ${details}`
+    }
+
+    return `${error.extensions.code}: ${error.message}`
+  }
+
+  return null
+}
+
+/**
+ * Query GraphQL and manage a simple state
+ */
+export function useQuery(
+  query: string,
+  variables: object = null,
+  handleError = null
+) {
+  const [state, setState] = useState<HookState>({
+    loading: true,
+    data: null,
+    error: null,
+  })
+
+  useEffect(() => {
+    graphQlFetch(query, variables)
+      .then(({ data, errors }) => {
+        setState({
+          loading: false,
+          data,
+          error: parseErrors(errors),
+        })
+      })
+      .catch(error => {
+        if (handleError) {
+          handleError(error)
+        }
+
+        setState({
+          ...state,
+          loading: false,
+          error,
+        })
+      })
+  }, [])
+
+  return state
+}
+
+/**
+ * Mutate GraphQL and manage a simple state
+ *
+ * @param {string} query GraphQL mutation query
+ * @param {function=} onSuccess Optional callback function, takes two arguments: data, error
+ */
+export function useMutation(
+  query: string,
+  onSuccess: (state?: object, error?: string) => void = null
+) {
+  const [state, setState] = useState({
+    loading: false,
+    error: null,
+    data: null,
+  })
+
+  const mutate = async variables => {
+    setState({ ...state, loading: true })
+
+    try {
+      const result = await graphQlFetch(query, variables)
+      setState({
+        loading: false,
+        data: result.data,
+        error: parseErrors(result),
+      })
+
+      if (onSuccess) {
+        return onSuccess(state.data, state.error)
+      }
+
+      return result
+    } catch (error) {
+      setState({
+        ...state,
+        loading: false,
+        error,
+      })
+
+      throw error
+    }
+  }
+
+  return [mutate, state]
+}
