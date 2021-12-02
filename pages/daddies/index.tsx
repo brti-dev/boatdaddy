01ad642aaf8f4453a850d91248797e2831cba87a
Matt Berti <mat.berti@gmail.com>
2021-12-02 22:37:44 +0800

    Daddies page skeleton

--- pages/daddies/index.tsx
+++ pages/daddies/index.tsx
@@ -4,11 +4,29 @@ import { UserList_data } from 'src/interfaces/api/user'
 import Layout from 'src/components/Layout'
 import ErrorPage from 'src/components/ErrorPage'
 import Loading from 'src/components/Loading'
+import userDataFragment from 'src/graphql/fragments/user-data'
+
+const DADDIES_QUERY = gql`
+  query {
+    userList(isBoatDaddy: true) {
+      users {
+        ...userData
+      }
+    }
+  }
+  ${userDataFragment}
+`
 
 export default function Daddies() {
+  const { data, error, loading } = useQuery<UserList_data>(DADDIES_QUERY)
+
+  if (error)
+    return <ErrorPage message={error.message ?? 'Something went wrong'} />
+
   return (
     <Layout title="Boat Daddies">
-      <p>daddies</p>
+      <h1>Boat Daddies</h1>
+      {!data || loading ? <Loading /> : <p>{JSON.stringify(data.userList)}</p>}
     </Layout>
   )
 }
