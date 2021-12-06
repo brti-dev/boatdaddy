8ceb08c7be4144bae92ce358df8bcb0e44f7453e
Matt Berti <mat.berti@gmail.com>
2021-12-06 20:54:02 +0800

    Merge components from starter repo

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,30 +1,31 @@
 import Tooltip from '@reach/tooltip'
 import '@reach/tooltip/styles.css'
-import { ReactHTMLElement } from 'react';
 
 import Button, { ButtonProps } from './Button'
 
 export type IconButtonProps = ButtonProps & {
-    tooltip?: string
+  tooltip?: string
 }
 
 function IconButton({ children, ...rest }: any) {
-    return (
-        <Button variant="icon" {...rest}>
-            {children}
-        </Button>
-    );
+  return (
+    <Button icon={true} {...rest}>
+      {children}
+    </Button>
+  )
 }
 
 function CustomIconButton({ tooltip, ...rest }: IconButtonProps) {
-    return (<>
-        {tooltip
-            ? (
-                <Tooltip label={tooltip}>
-                    <IconButton aria-label={tooltip} {...rest} />
-                </Tooltip>
-            )
-            : <IconButton {...rest} />}
-    </>)
+  return (
+    <>
+      {tooltip ? (
+        <Tooltip label={tooltip}>
+          <IconButton aria-label={tooltip} {...rest} />
+        </Tooltip>
+      ) : (
+        <IconButton {...rest} />
+      )}
+    </>
+  )
 }
 export default CustomIconButton
