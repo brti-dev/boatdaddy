f69ec0f1db42b3102436a5b42e55fbb2201d667e
Matt Berti <mat.berti@gmail.com>
2022-02-13 21:55:44 +0800

    Hail function gets users within map bounds

--- pages/hail.tsx
+++ pages/hail.tsx
@@ -1,55 +1,62 @@
 import { useEffect, useRef, useState } from 'react'
-import { useQuery, gql } from '@apollo/client'
-import { useRouter } from 'next/router'
+import { useLazyQuery, gql } from '@apollo/client'
+import router, { useRouter } from 'next/router'
 import Link from 'next/link'
 
+import {
+  NearbyDrivers_variables,
+  NearbyDrivers_data,
+  User,
+} from 'src/interfaces/api/user'
 import { useUser } from 'src/context/user-context'
 import Layout from 'src/components/Layout'
 import Button from 'src/components/Button'
-import Map, { MapMarker } from 'src/components/Map'
+import Map, { MapMarker, ViewportState } from 'src/components/Map'
 import classes from 'src/styles/map.module.scss'
 
-// const DADDIES_QUERY = gql`
-//   query {
-//     userList(isBoatDaddy: true) {
-//       users {
-//         id
-//         username
-//         image
-//         createdAt
-//         profile {
-//           name
-//           birthday
-//           isBoatDaddy
-//           boatName
-//           boatImage
-//         }
-//       }
-//     }
-//   }
-// `
+const NEARBY_DRIVERS_QUERY = gql`
+  query nearbyDrivers($bounds: String) {
+    nearbyDrivers(bounds: $bounds) {
+      id
+      username
+    }
+  }
+`
 
 const HAIL = ['DonSoprano', 'G.O.B.', 'CommodoreHull', 'Fuuuuuuu']
 
 export default function Hail() {
-  const router = useRouter()
+  const {
+    query: { driver },
+  } = useRouter()
   const user = useUser()
-  const [mapState, setMapState] = useState(null)
+  const [mapState, setMapState] = useState<ViewportState>(null)
+  const [getNearby, nearby] = useLazyQuery<
+    NearbyDrivers_data,
+    NearbyDrivers_variables
+  >(NEARBY_DRIVERS_QUERY)
 
-  const [driver, setDriver] = useState(router.query?.driver?.slice(1))
+  const setDriver = driver =>
+    router.replace(driver ? '/hail?driver=' + driver : '/hail', undefined, {
+      shallow: true,
+    })
 
   useEffect(() => {
-    console.log('driver effect', driver)
-  }, [driver])
+    console.log('map state effect', mapState)
+    if (!mapState?.bounds) {
+      return
+    }
+    getNearby({
+      variables: {
+        bounds: `[[${mapState.bounds[0][0]},${mapState.bounds[0][1]}],[${mapState.bounds[1][0]},${mapState.bounds[1][1]}]]`,
+      },
+    })
+  }, [mapState])
 
   return (
     <Layout title="Hail A Boat Daddy" showFooter={false}>
       <div className={classes.map}>
-        <Map
-          latitude={user.data.latitude}
-          longitude={user.data.longitude}
-          onChange={setMapState}
-        >
+        <Map onChange={setMapState}>
           <MapMarker
             latitude={user.data.latitude}
             longitude={user.data.longitude}
@@ -73,18 +80,54 @@ export default function Hail() {
               </Button>
             </div>
           ) : (
-            <div>
-              {HAIL.map(driver => (
-                <Link href={`/hail/@${driver}`} replace shallow>
-                  {driver}
-                </Link>
-              ))}
-            </div>
+            <NearbyList
+              data={nearby.data as NearbyDrivers_data}
+              error={nearby.error}
+              loading={nearby.loading}
+            />
           )}
         </>
-        <>{mapState ? JSON.stringify(mapState) : '...'}</>
       </div>
     </Layout>
+  )
+}
+
+function NearbyList({
+  data,
+  error,
+  loading,
+}: {
+  data: NearbyDrivers_data
+  error: any
+  loading: boolean
+}) {
+  if (error) {
+    console.error(error)
+    return <>Something went wrong</>
+  }
+
+  if (loading) {
+    return <>...</>
+  }
+
+  if (!data || !data?.nearbyDrivers?.length) {
+    return <>No drivers nearby</>
+  }
+
+  return (
+    <>
+      {data.nearbyDrivers.map(user => (
+        <DriverCard user={user} key={user.id} />
+      ))}
+    </>
+  )
+}
+
+function DriverCard({ user }: { user: User }) {
+  return (
+    <div>
+      <Button to={`/hail/?driver=${user.username}`}>{user.username}</Button>
+    </div>
   )
 }
 
